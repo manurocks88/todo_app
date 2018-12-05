c52880197c98817e5ae47b36be4e2974c86aead2
unknown <elzh88@yahoo.com.sg>
2018-12-05 20:36:12 +0800

    Creates header component

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import Header from './Components/header';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <Header textIwant={'hello'}/>
+      <Header textIwant={'Goodbye'}/>
+              <Text style={styles.bigblue}>DO YOU DA WAE</Text>
+        <Text>( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)( ͡° ͜ʖ ͡°)</Text>
+        <Avatar
+  size="xlarge"
+  rounded
+  source={{uri: "https://i.kym-cdn.com/entries/icons/original/000/025/067/ugandanknuck.jpg"}}
+  onPress={() => console.log("Works!")}
+  activeOpacity={0.7}
+/>
+        <Button 
+          title= {'Hello'} 
+          onPress={() => { console.log('Hello') ; }}
+          color= 'green'
+          />
       </View>
     );
   }
@@ -17,5 +34,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+
+bigblue: {
+  color: 'red',
+  fontWeight : 'bold',
+  fontSize :  30,
+  alignItems: 'center',
   },
 });
