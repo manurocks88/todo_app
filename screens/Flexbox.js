325b19d385620c57952b56eee07246c12b0b1066
unknown <elzh88@yahoo.com.sg>
2018-12-10 21:58:51 +0800

    List view added

--- screens/Flexbox.js
+++ screens/Flexbox.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
 
 class Flexbox extends React.Component{
     render(){
-        return(<Text>Flexbox App</Text>);
+            
+        return(
+            <ScrollView>
+                <View style = {styles.container1} />
+            </ScrollView>
+        );
+    
     }
 }
+
+const styles = StyleSheet.create({
+    
+    container1: {
+        padding: 100,
+        backgroundColor:'blue',
+    },  
+});
+
 export default Flexbox;
--- screens/Todo.js
+++ screens/Todo.js
@@ -3,7 +3,41 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 class Todo extends React.Component{
     render(){
-        return(<Text>Todo App</Text>);
+
+        return(
+            <View 
+                style={{
+                    flex: 1, 
+                    flexDirection: 'row-reverse',
+                    flexWrap: 'wrap'
+                    }}>
+
+                <View 
+                    style={{
+                        flex: 1,
+                        backgroundColor: 'powderblue'
+                    }}
+                />
+
+
+                <View 
+                    style={{
+                        flex: 1, 
+                        backgroundColor: 'skyblue'
+                    }} 
+                />
+
+
+
+                    <View 
+                        style={{
+                            flex: 1,
+                            backgroundColor: 'steelblue'
+                        }} 
+                    />
+            </View>
+
+        );  
     }
 }
 export default Todo;
