fc2fbef50a21aca4d50853aaf92f4ac80f79ef35
unknown <elzh88@yahoo.com.sg>
2018-12-10 20:56:53 +0800

    Adds flexbox screen

---
+++ screens/Todo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+class Todo extends React.Component{
+    render(){
+        return(<Text>Todo App</Text>);
+    }
+}
+export default Todo;
